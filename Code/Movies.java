ea0ebb8578a07cba66d44f91c4324e874792c728
Sofie Højgaard <Sofie Hjgaard>
2015-12-14 13:19:14 +0100

    upload code

---
+++ Code/Movies.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Movies {
+    
+    static String[] M = {"A", "B", "C", "D", "E", "F", "G"};
+    static int count = 0;
+
+
+    public static void main(String[] args) {
+        combinations(0, M, "", 8);
+        System.out.println("Antal:" + count);
+
+    }
+    
+    public static void combinations(int index, String[] movies, String temp, int m){
+        String newTemp;        
+        
+        for(int i = index; i < movies.length; i++){
+
+            newTemp = temp + movies[i];
+            
+            if (newTemp.length() < m)
+                combinations(i + 1, movies, newTemp, m);
+            else{
+               System.out.println(newTemp);
+               count++;
+            }     
+        }        
+    }
+}
+    
+
+   
