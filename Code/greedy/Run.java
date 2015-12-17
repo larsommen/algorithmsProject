d0544aca1e65cc74d0c673b241137ec34f693023
Andreas Rangholm <andreas@rangholm.dk>
2015-12-17 13:13:03 +0100

    Added to greedy folder and added detVirker3.csv to data folder

---
+++ Code/greedy/Run.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.File;
+
+/**
+ *
+ * @author arangholm
+ */
+public class Run{
+    
+    public static void main(String[] args){
+        String actorFile = "/Users/arangholm/algorithmsProject/Code/greedy/1.csv";
+        String movieFile = "/Users/arangholm/algorithmsProject/Code/greedy/2.csv";
+        String actorMovieFile = "detVirker3.csv";
+        
+        Combinations c = new Combinations(actorFile, movieFile, actorMovieFile, 3);
+        c.run();
+        //Phase 1
+        c.findCombinations(3);
+        //Phase 2
+        c.sortScreenings();
+        //Phase 3
+        c.greedySelect();
+    }    
+    
+}
