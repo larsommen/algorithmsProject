9f67a532c4cedc7d9fa1c3deb487742adca0a898
Andreas Rangholm <andreas@rangholm.dk>
2015-12-17 18:52:47 +0100

    Nothing

--- Code/greedy/Run.java
+++ Code/greedy/Run.java
@@ -13,15 +13,11 @@ import java.io.File;
 public class Run{
     
     public static void main(String[] args){
-        String actorFile = "/Users/arangholm/algorithmsProject/Code/greedy/1.csv";
-        String movieFile = "/Users/arangholm/algorithmsProject/Code/greedy/2.csv";
-        String actorMovieFile = "asr.csv";
+        String actorFile = "ActorsLarge.csv";
+        String movieFile = "MoviesLarge.csv";
+        String actorMovieFile = "1000RelLarge.csv";
         
-<<<<<<< HEAD
-        Combinations c = new Combinations("../../data/ActorsLarge.csv", "../../data/MoviesLarge.csv", "../../data/30RelLarge.csv", 3);
-=======
         Combinations c = new Combinations(actorFile, movieFile, actorMovieFile, 3);
->>>>>>> 0eae80c3c57e2f7c9a6c85241936911162a655f1
         c.run();
         //Phase 1
         c.findCombinations(3);
